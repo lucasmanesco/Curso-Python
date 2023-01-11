1f3408b1de7f8ce2837bd3a77d48be5b3a6f3dc7
Lucas Soares Manesco <100358020+lucasmanesco@users.noreply.github.com>
2023-01-11 12:50:47 -0300

    finalizei exercício calculadora

--- Secao3/aula66_ex-calculadora.py
+++ Secao3/aula66_ex-calculadora.py
@@ -6,14 +6,26 @@ while True:
     n1 = input('Informe o primeiro valor: ')
     n2 = input('Informe o segundo valor: ')
 
+    numeros_validos = None
+
     try:
         fn1 = float(n1)
         fn2 = float(n2)
+        numeros_validos = True
     except:
+        numeros_validos = None
+    
+    if numeros_validos == None:
         print('Digite valores numéricos.')
-
-
+        continue
+    
     operacao = input('Qual operação deseja realizar?\nA - Adição\nS - Subtração\nM - Multiplicação\nD - Divisão\n->')
+
+    operadores_validos = 'ASMD'
+
+    if operacao.upper() not in operadores_validos.upper() or len(operacao) > 1:
+        print('Operação inválida.')
+        continue
 
     if operacao.upper() == 'A':
         print(f'Resultado = {fn1 + fn2}')
@@ -23,10 +35,8 @@ while True:
         print(f'Resultado = {fn1 * fn2}')
     elif operacao.upper() == 'D':
         print(f'Resultado = {fn1 / fn2}')
-    else:
-        print('Você não selecionou nenhuma operação.')
 
     opcao = input('Deseja fazer outro cálculo? (S)/(N)').upper().startswith('S')
 
-    if opcao is True:
+    if opcao is not True:
         break
