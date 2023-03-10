486dda0821a6da733074c10f27bfbe561528b927
Lucas Soares Manesco <100358020+lucasmanesco@users.noreply.github.com>
2023-03-10 15:46:21 -0300

    estudos-10-03

--- Secao4/aula175_groupby.py
+++ Secao4/aula175_groupby.py
@@ -1,4 +1,7 @@
 # groupby - agrupando valores (itertools)
+# dados precisam estar ordenados para agrupar corretamente
+
+from itertools import groupby
 
 alunos = [
     {'nome': 'Luiz', 'nota': 'A'},
@@ -11,3 +14,16 @@ alunos = [
     {'nome': 'André', 'nota': 'A'},
     {'nome': 'Anderson', 'nota': 'C'},
 ]
+
+
+def ordena(aluno):
+    return aluno['nota']
+
+
+alunos_agrupados = sorted(alunos, key=ordena)
+grupos = groupby(alunos_agrupados, key=ordena)
+
+for chave, grupo in grupos:
+    print(chave)
+    for aluno in grupo:
+        print(aluno)
---
+++ Secao4/aula176_map-partial-genType.py
@@ -0,0 +1,53 @@
+# map - para mapear dados
+
+from functools import partial
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+
+def aumentar_porcentagem(valor, porcentagem):
+    return round(valor * porcentagem, 2)
+
+
+def muda_preco_de_produto(produto):
+    return {**produto, 'preco': aumentar_dez_porcento(produto['preco'])}
+
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+aumentar_dez_porcento = partial(
+    aumentar_porcentagem, porcentagem=1.1
+)
+
+# novos_produtos = [
+#     {**p, 'preco': aumentar_dez_porcento(p['preco'])} for p in produtos
+# ]
+
+novos_produtos = map(
+    muda_preco_de_produto,
+    produtos
+)
+
+print_iter(produtos)
+print_iter(novos_produtos)
+
+print(novos_produtos)
+print(hasattr,(novos_produtos, '__iter__'))
+print(hasattr,(novos_produtos, '__next__'))
+
+print(list(novos_produtos))
+
+print(list(map(
+    lambda x: x*3, 
+    [1, 2, 3, 4]
+        )
+    )
+)
