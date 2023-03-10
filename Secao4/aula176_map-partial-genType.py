2914dde219331e61c30576e0cb413f272f5e719b
Lucas Soares Manesco <100358020+lucasmanesco@users.noreply.github.com>
2023-03-10 15:51:15 -0300

    correcao-map

--- Secao4/aula176_map-partial-genType.py
+++ Secao4/aula176_map-partial-genType.py
@@ -23,14 +23,17 @@ produtos = [
     {'nome': 'Produto 4', 'preco': 69.90},
 ]
 
+# closure
 aumentar_dez_porcento = partial(
     aumentar_porcentagem, porcentagem=1.1
 )
 
+# list comprehension
 # novos_produtos = [
 #     {**p, 'preco': aumentar_dez_porcento(p['preco'])} for p in produtos
 # ]
 
+# map
 novos_produtos = map(
     muda_preco_de_produto,
     produtos
