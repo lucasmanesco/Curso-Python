4e38fbad32ef753476687f68bd1466448d928793
Lucas Soares Manesco <100358020+lucasmanesco@users.noreply.github.com>
2023-03-29 16:50:13 -0300

    estudos-29-03

---
+++ Secao5/aula245_metodo_call.py
@@ -0,0 +1,17 @@
+# Método especial __call__
+# callable é algo que pode ser executado com parênteses
+# Em classes normais, __call__ faz a instância de uma
+# classe "callable".
+
+class CallMe:
+    def __init__(self, phone):
+        self.phone = phone
+
+    def __call__(self, nome):
+        print(nome, 'está chamando:', self.phone)
+        return 'ring-ring'
+
+
+call1 = CallMe('19988410611')
+retorno = call1('Lucas Manesco')
+print(retorno)
