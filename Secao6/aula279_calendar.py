0d18f5ad1bdaa925465057be634e445fdde19687
Lucas Soares Manesco <100358020+lucasmanesco@users.noreply.github.com>
2023-04-04 16:38:00 -0300

    estudos-04-04

---
+++ Secao6/aula279_calendar.py
@@ -0,0 +1,32 @@
+# Usando calendar para calendários e datas
+# https://docs.pyhton.org/3/library/calendar.html
+# calendar é usado para coisas genéricas de calendários e datas.
+# Com calendar, você pode saber coisas como:
+# - Qual o último dia do mês (ex.: monthrange)
+# - Qual o nome e número do dia de determinada data (ex.: weekday)
+# - Criar um calendário em si (ex.: monthcalendar)
+# - Trabalhar com coisas específicas de calendários (ex.: calendar, month)
+# Por padrão dia da semana começa em 0 até 6
+# 0 = segunda-feira | 6 = domingo
+import calendar
+
+print(calendar.calendar(2023))
+print(calendar.month(2023, 4))
+
+print(calendar.weekday(2023, 4, 4))
+print(list(enumerate(calendar.day_name)))
+
+numero_primeiro_dia, ultimo_dia = calendar.monthrange(2023, 4)
+print(calendar.day_name[numero_primeiro_dia])
+print(calendar.day_name[calendar.weekday(2023, 4, ultimo_dia)])
+
+print(calendar.monthcalendar(2023, 4))
+
+for week in calendar.monthcalendar(2023, 4):
+    print(list(enumerate(week)))
+
+for week in calendar.monthcalendar(2023, 4):
+    for day in week:
+        if day == 0:
+            continue
+        print(day)
