dfde7115a68d4dae93b4043a1fff124d33ece0e4
Lucas Soares Manesco <100358020+lucasmanesco@users.noreply.github.com>
2023-04-06 16:23:34 -0300

    estudos-06-04

---
+++ Secao6/aula286_os_shutil.py
@@ -0,0 +1,6 @@
+os + shutil - Mover, copiar e apagar arquivos
+Mover/Renomear -> shutil.move
+Mover/Renomear -> os.rename
+Copiar -> shutil.copy
+Apagar -> os.unlink
+Apagar diretório recursivamente -> shutil.rmtree
